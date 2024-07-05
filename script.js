79f7b16b69ebd227c5b6f52663321a4435b9c0a1
Kalvin Liang Wu <kalvinliang123@gmail.com>
2024-07-05 19:44:08 -0400

    fix the loop where i use wrong variable in nested loop, fix the tied(), add in more debug print out

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function GameBoard() {
     const board = [];
     for (let i = 0; i < row; i++) {
         board[i] = [];
-        for (let j = 0; i < col; j++) {
+        for (let j = 0; j < col; j++) {
             board[i].push(Cell());
         }
     }
@@ -21,6 +21,15 @@ function GameBoard() {
     const getBoard = () => board;
 
     const placeMark = (r, c, player) => {
+        if (r < 0 || r >= row) {
+            alert("Invalid row number");
+            return false;
+        }
+
+        if (c < 0 || c >= col) {
+            alert("Invalid col number");
+            return false;
+        }
         if (board[r][c].getValue() === 0) {
             board[r][c].setValue(player);
             return true;
@@ -35,39 +44,56 @@ function GameBoard() {
 
 
     const tied = () => {
-        board.forEach((row) =>{
-            row.forEach(cell => {
-                if (cell.getValue() === 0) {
-                    return false;
-                }
-            });
-        });
-        return true;
-    }
-    
-    const win = (playerToken) => {
-        const otherPlayer = player_token_sum - playerToken;
-        let acc = 0;
-        // row
         for (let i = 0; i < row; i++) {
             for (let j = 0; j < col; j++) {
-                if (board[i][j].getValue === otherPlayer) {
+                if (board[i][j].getValue() === 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    const win = (playerToken) => {
+        console.log("Player token: "  + playerToken);
+        const otherPlayer = player_token_sum - playerToken;
+        console.log("other Player token: "  + otherPlayer);
+        let acc = 0;
+        // row
+        let val;
+        for (let i = 0; i < row; i++) {
+            for (let j = 0; j < col; j++) {
+                val = board[i][j].getValue();
+                if (val === otherPlayer) {
                     break;
                 }
-                acc++;
+
+                if (val === playerToken) {
+                    acc++;
+                }
             }
-            if (acc === 3) return true;
+            if (acc === 3) {
+                console.log("won in row");
+                return true;
+            }
             acc = 0;
         }
         // column
         for (let j = 0; j < col; j++) {
             for (let i = 0; i < row; i++) {
-                if (board[i][j].getValue === otherPlayer) {
+                val = board[i][j].getValue();
+                if (val === otherPlayer) {
                     break;
                 }
-                acc++;
+
+                if (val === playerToken) {
+                    acc++;
+                }
             }
-            if (acc === 3) return true;
+            if (acc === 3) {
+                console.log("won in col");
+                return true;
+            }
             acc = 0;
         }
         // major
@@ -77,7 +103,7 @@ function GameBoard() {
         return false;
     }
     const gameOver = (player_token="") => {
-        if (player === "")
+        if (player_token === "")
             return win(playerOne_token) || win(playerTwo_token) || tied();
         else {
             return win(player_token) || tied();
@@ -94,16 +120,13 @@ function GameBoard() {
 
 
 function Cell() {
-    value = 0;
-
+    let value = 0;
     getValue = () => {
         return value;
     };
-
     setValue = (player) => {
         value = player;
     };
-
     return {
         getValue,
         setValue,
@@ -111,9 +134,10 @@ function Cell() {
 }
 
 
-// this method is use to intialize and control the game flow.
+// // this method is use to intialize and control the game flow.
 function GameController(playerOneName = "Player One", playerTwoName = "Player Two") {
     
+    console.log("hi");
     const players = [
         {
             name: playerOneName,
@@ -125,16 +149,16 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
             token: playerTwo_token,
         }
     ];
-    const gameBoard = GameBoard();
-    let activePlayer = players[0];
+    const gameBoard = GameBoard();  
+    let activePlayer = players[0]
     const get_active_player = () => activePlayer;
     const switch_active_player = () => {
-        activePlayer =  activePlayer === player[0]? player[1] : player[0];
+        activePlayer =  activePlayer === players[0]? players[1] : players[0];
     }
     
     const printNewRound = () => {
         gameBoard.printBoard();
-        console.log(`New round; ${get_active_player}'s turn`);
+        console.log(`New round; ${get_active_player().name}'s turn`);
     }
 
     /**
@@ -145,15 +169,15 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
      * @returns   true indicate the game ended, false indicate continue
      */
     const playRound = (r, c) => {
-        console.log(`${activePlayer} add mark to (${r},${c})`);   
-        let ret = gameBoard.placeMark(r,c, get_active_player());
+        console.log(`${get_active_player().name} add mark to (${r},${c})`);   
+        let ret = gameBoard.placeMark(r,c, get_active_player().token);
         
         if (!ret) {
             console.log("Invalid move; please place the mark in empty space");
             return false;
         } 
-
-        if (gameBoard.gameOver(get_active_player)) {
+        if (gameBoard.gameOver(get_active_player().token)) {
+            gameBoard.printBoard();
             console.log(`${get_active_player} won!`);
             return true;
         }
